21f6a2af6c5d94610266e2ab96d174eceeea4b80
xyanxuyi1 <415785815@qq.com>
2022-09-27 21:15:17 +0800

    使用loadBalancer替换ribbon

--- springcloudalibaba/order/src/main/java/com/order/OrderApplication.java
+++ springcloudalibaba/order/src/main/java/com/order/OrderApplication.java
@@ -1,12 +1,9 @@
 package com.order;
 
-import com.ribbon.RibbonRandomRuleConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
--- springcloudalibaba/order/src/main/java/com/ribbon/RibbonRandomRuleConfig.java
+++ springcloudalibaba/order/src/main/java/com/ribbon/RibbonRandomRuleConfig.java
@@ -1,23 +1,23 @@
-package com.ribbon;
-
-import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.RandomRule;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * @ClassName RibbonRandomRuleConfig
- * @Description
- * @Author 徐仡
- * @Date 2022/9/27 15:50
- * @Version 1.0
- */
-@Configuration
-public class RibbonRandomRuleConfig {
-
-
-    @Bean
-    public IRule iRule() {
-        return new RandomRule();
-    }
-}
+//package com.ribbon;
+//
+//import com.netflix.loadbalancer.IRule;
+//import com.netflix.loadbalancer.RandomRule;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * @ClassName RibbonRandomRuleConfig
+// * @Description
+// * @Author 徐仡
+// * @Date 2022/9/27 15:50
+// * @Version 1.0
+// */
+//@Configuration
+//public class RibbonRandomRuleConfig {
+//
+//
+//    @Bean
+//    public IRule iRule() {
+//        return new RandomRule();
+//    }
+//}
