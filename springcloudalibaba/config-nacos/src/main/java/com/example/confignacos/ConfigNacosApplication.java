d089c94be9ea281a82b78db5b93aa8d1dd4fc065
xyanxuyi1 <415785815@qq.com>
2022-09-28 13:41:41 +0800

    nacos config client读取配置

---
+++ springcloudalibaba/config-nacos/src/main/java/com/example/confignacos/ConfigNacosApplication.java
@@ -0,0 +1,22 @@
+package com.example.confignacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.concurrent.TimeUnit;
+
+@SpringBootApplication
+public class ConfigNacosApplication {
+
+    public static void main(String[] args) throws InterruptedException {
+        ConfigurableApplicationContext run = SpringApplication.run(ConfigNacosApplication.class, args);
+        for (;;) {
+            String username = run.getEnvironment().getProperty("user.name");
+            String userage = run.getEnvironment().getProperty("user.age");
+            System.out.println("user name: " + username + ": user age: " + userage);
+            TimeUnit.SECONDS.sleep(1);
+        }
+    }
+
+}
---
+++ springcloudalibaba/config-nacos/src/test/java/com/example/confignacos/ConfigNacosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.confignacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigNacosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
