da3b6368fee5863ed1a431a820542ba419fba413
xyanxuyi1 <415785815@qq.com>
2022-09-29 22:34:37 +0800

    sentinel热点参数流控

--- springcloudalibaba/order-openfegin-sentinel/src/main/java/com/example/orderopenfeginsentinel/controller/OrderOpenFeginSentinelController.java
+++ springcloudalibaba/order-openfegin-sentinel/src/main/java/com/example/orderopenfeginsentinel/controller/OrderOpenFeginSentinelController.java
@@ -1,7 +1,10 @@
 package com.example.orderopenfeginsentinel.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.example.orderopenfeginsentinel.fegin.StockFeginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +29,23 @@ public class OrderOpenFeginSentinelController {
 
         return "hello fegin: "+ s;
     }
+
+    /**
+     * @Description 热点规则必须使用  @SentinelResource使用
+     * @Author xuge
+     * @Date 2022/9/29 22:30
+     * @Version 1.0
+     */
+    @RequestMapping("/get/{id}")
+    @SentinelResource(value = "getById", blockHandler = "HotBlockHandler")
+    public String getById(@PathVariable("id") Integer id) throws InterruptedException {
+        System.out.println("正常访问");
+        return "正常访问";
+    }
+
+
+    public String HotBlockHandler(@PathVariable("id") Integer id, BlockException e) throws InterruptedException {
+        return "热点异常处理";
+    }
+
 }
